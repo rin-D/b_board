a6eabd8260b235c035ca9b64393478452ea98cb3
rinDwurm <thayashida329@gmail.com>
2018-07-06 17:21:04 +0900

    機能を盛り込むために大幅改良

---
+++ dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+//データベースの接続と選択
+require_once('config.php');
+
+$mysqli = new mysqli($host, $username, $password, $dbname);
+if ($mysqli->connect_error) {
+	error_log($mysqli->connect_error);
+	exit;
+}
--- index.php
+++ index.php
@@ -1,111 +1,78 @@
 <?php
-
-$db_host = 'localhost';
-$db_name = 'board_db';
-$db_user = 'board_user';
-$db_pass = 'board_pass';
-
-$dataFile = 'bbs.dat';
-
-#エスケープ関数の作成
-function h($s) {
-    return htmlspecialchars($s, ENT_QUOTES, 'utf-8');
-}
- 
-// データベースへ接続する
-$link = mysqli_connect( $db_host, $db_user, $db_pass, $db_name );
-if ( $link !== false ) {
-//MySQLがつながった時の処理
-
-#入力値の確認
-	if ($_SERVER['REQUEST_METHOD']=='POST' &&
-    	isset($_POST['message']) &&
-    	isset($_POST['user'])){
-
-    	$message = trim($_POST['message']);
-    	$user = trim($_POST['user']);
-
-	        #message がなかったときの処理
-    	if ($message !== ''){
-        	#$user がなかったときの処理
-        	$user = ($user === '') ? 'noname' : $user;
-
-	        #$message と $user に \t が含まれてた場合それを置き換える
-    	    $message = str_replace("\t", ' ', $message);
-        	$user = str_replace("\t", ' ', $user);
-
-        	$postedAt = date('Y-m-d H-i-s');
-
-        	// $newData = $message . "\t" . $user . "\t" . $postedAt. "\n";
-
-        	// $fp = fopen($dataFile, 'a');
-        	// fwrite($fp, $newData);
-        	// fclose($fp);
-        	$query = " INSERT INTO board ( "
-                   . "    name , "
-                   . "    comment , "
-                   . "    time , "
-                   . " ) VALUES ( "
-                   . "'" . mysqli_real_escape_string( $link, $user ) ."', "
-                   . "'" . mysqli_real_escape_string( $link, $message ) . "', "
-                   . "'" . mysqli_real_escape_string( $link, $postedAt ) . "'"
-                   ." ) ";
- 
-            $res   = mysqli_query( $link, $query );
-            
-            if ( $res !== false ) {
-                $msg = '書き込みに成功しました';
-            }else{
-                $err_msg = '書き込みに失敗しました';
-            }	
-    	}else{
-    		$err_msg = 'コメントを記入してください';
-    	}
-	}
-
-	$query  = "SELECT id, name, comment FROM board";
-    $res    = mysqli_query( $link,$query );
-    $data = array();
-    while( $row = mysqli_fetch_assoc( $res ) ) {
-        array_push( $data, $row);
-    }
-    arsort( $data );
-
-} else {
-    echo "データベースの接続に失敗しました。";
+//ユーザーがログイン済みの場合にhome.php(マイページ)に飛ばす
+ob_start();
+session_start();
+if( isset($_SESSION['user']) != "") {
+	header("Location: home.php");
 }
 
-// $posts = file($dataFile, FILE_IGNORE_NEW_LINES);
-
-// $posts = array_reverse($posts);
-
-// データベースへの接続を閉じる
-mysqli_close( $link );
+include_once 'dbconnect.php';
 ?>
 
-<!DOCTYPE html>
+<?php
+// ログインボタンが押された場合の処理
+if(isset($_POST['login'])) {
+
+	$email = $mysqli->real_escape_string($_POST['email']);
+	$password = $mysqli->real_escape_string($_POST['password']);
+
+	// クエリーの実行
+	$query = "SELECT * FROM users WHERE email='$email'";
+	$result = $mysqli->query($query);
+	if (!$result) {
+		print('クエリーが失敗しました。' . $mysqli->error);
+		$mysqli->close();
+		exit();
+	}
+
+	// パスワード(暗号化済み）とユーザーIDの取り出し
+	while ($row = $result->fetch_assoc()) {
+		$db_hashed_pwd = $row['password'];
+		$user_id = $row['user_id'];
+	}
+
+	// データベースの切断
+	$result->close();
+
+	// ハッシュ化されたパスワードがマッチするかどうかを確認
+	// 一致したらhome.phpに飛ばす
+	// 一致しなかったら再度入力を促す
+
+	if (password_verify($password, $db_hashed_pwd)) {
+		$_SESSION['user'] = $user_id;
+		header("Location: home.php");
+		exit;
+	} else { ?>
+		<div class="alert alert-danger" role="alert">メールアドレスとパスワードが一致しません。</div>
+	<?php }
+} ?>
+
+<!DOCTYPE HTML>
 <html lang="ja">
 <head>
-    <meta charset="utf-8">
-    <title>BBS</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>PHPのログイン機能</title>
+<link rel="stylesheet" href="style.css">
+<!-- Bootstrap読み込み（スタイリングのため） -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+</head>
 </head>
 <body>
-    <h1>BBS</h1>
-    <form action="" method="post">
-        message: <input type="text" name="message">
-        user: <input type="text" name="user">
-        <input type="submit" value="投稿">
-    </form>
-    <h2>投稿一覧(<?php echo count($posts); ?>件)</h2>
-    <ul>
-        <?php if (count($posts)) : ?>
-            <?php foreach ($posts as $post) : ?>
-            <?php list($message, $user, $postedAt) = explode("\t", $post); ?>
-                <li><?php echo h($message); ?> (<?php echo h($user); ?>) - <?php echo h($postedAt); ?></li>
-            <?php endforeach; ?>
-        <?php else : ?>
-            <li>まだ投稿はありません</li>
-        <?php endif; ?>
-    </ul>
+<div class="col-xs-6 col-xs-offset-3">
+
+<form method="post">
+	<h1>ログインフォーム</h1>
+	<div class="form-group">
+		<input type="email"  class="form-control" name="email" placeholder="メールアドレス" required />
+	</div>
+	<div class="form-group">
+		<input type="password" class="form-control" name="password" placeholder="パスワード" required />
+	</div>
+	<button type="submit" class="btn btn-default" name="login">ログインする</button>
+	<a href="register.php">会員登録はこちら</a>
+</form>
+
+</div>
 </body>
 </html>
